df39a9bab3fef2e65d0e92cd007d59066609282a
shubhojit <royshubhojit@gmail.com>
2019-03-03 12:39:56 +1100

    initial commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserModule } from './user/user.module';
+import { MessageModule } from './message/message.module';
+import { WelcomeComponent } from './home/welcome/welcome.component';
+import { PagenotfoundComponent } from './home/pagenotfound/pagenotfound.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
+import { ProductDetailComponent } from './products/product-detail/product-detail.component';
+import { HeaderComponent } from './childcomponents/header/header.component';
+import { FooterComponent } from './childcomponents/footer/footer.component';
+import { PrivacyPolicyComponent } from './childcomponents/privacy-policy/privacy-policy.component';
+import { TermsOfUseComponent } from './childcomponents/terms-of-use/terms-of-use.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    PagenotfoundComponent,
+    ProductListComponent,
+    ProductDetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    PrivacyPolicyComponent,
+    TermsOfUseComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    UserModule,
+    MessageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
